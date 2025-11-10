a409abde4034fe52be1053b10c0f61af00f5f335
Azhan Ali <fe4rtoxin45@gmail.com>
2025-11-10 21:13:30 +0500

    Add a function to interrogate the suspects

--- src/lib/logic.cpp
+++ src/lib/logic.cpp
@@ -10,7 +10,7 @@ int score;
 std::string currentLocation;
 std::string criminal;
 
-// Vector to store locations and suspects
+// Vector to store locations
 std::vector<std::string> locations = { "Study Room", "Kitchen", "Garden", "TV Lounge", "Basement" };
 std::vector<std::string> suspects = { "Ali", "Maria", "John" };
 
@@ -33,4 +33,26 @@ void movement(const std::string location)
 {
 	currentLocation = location;
 	energy -= 10;
+}
+
+// Function to interrogate the suspects
+std::string Interrogate(const std::string suspect_name)
+{
+	int reaction = rand() % 4;
+	if (reaction == 0)
+	{
+		return suspect_name + " seems nervous.";
+	}
+	else if (reaction == 1)
+	{
+		return suspect_name + " avoids your question.";
+	}
+	else if (reaction == 2)
+	{
+		return suspect_name + "'s story doesn't add up.";
+	}
+	else
+	{
+		return suspect_name + "answers your questions confidently and denies everything.";
+	}
 }
